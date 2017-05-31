19d72055553dd8996e7f14d67867d7b05d05c649
Antonius Yonathan <atomicbomber.email@gmail.com>
2017-05-31 12:21:59 +0700

    Fixed a bug in the model's relationship definition.

--- app/Item.php
+++ app/Item.php
@@ -8,7 +8,7 @@ class Item extends Model
 {
     public function category()
     {
-        return $this->hasOne("App\Category");
+        return $this->belongsTo("App\Category");
     }
 
     public function invoiceItems()
